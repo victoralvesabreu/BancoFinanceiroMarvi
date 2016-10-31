062c8e2be4e0fb71900e0c2b95b8332241146386
victor alves abreu <victoralvesabreu@outlook.com>
2016-10-31 19:39:10 -0200

    CRUD Ready

--- src/crud/UsuarioCRUD.java
+++ src/crud/UsuarioCRUD.java
@@ -5,10 +5,33 @@
  */
 package crud;
 
+import domain.Usuario;
+import database.Database;
+import java.util.ArrayList;
+
 /**
  *
- * @author tognere
+ * @author victor alves abreu
  */
 public class UsuarioCRUD {
+    public void inserir(Usuario usuario){
+        Database.listaUsuario.add(usuario);
+    }
     
+    public ArrayList<Usuario> ler(){
+        return Database.listaUsuario;
+    }
+    
+    public Usuario ler(int id){
+        for(Usuario u: Database.listaUsuario){
+            if (u.getId() == id) {
+                return u;
+            }
+        }
+        return null;
+    }
+    
+    public void remover(Usuario usuario){
+        Database.listaUsuario.remove(usuario);
+    }
 }
